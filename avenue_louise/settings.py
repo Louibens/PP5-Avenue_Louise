3e12797fd418ea8c82656f26865ea4beb881a4e1
Louise Benson <louise.benson@gmail.com>
2023-09-28 17:16:38 +0000

    Add header including logo, links and search form

--- avenue_louise/settings.py
+++ avenue_louise/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-54q^t1c&)1t^s6gzkh*1j(x=0!h_rr-b)-&*(ck803@(1wn)j(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-louibens-pp5avenuelouis-t61ayspmsnx.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-louibens-pp5avenuelouis-t61ayspmsnx.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
