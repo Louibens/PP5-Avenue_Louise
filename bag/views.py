5e1d09190edac68aae187d06ffffc4e2ee69829e
Louise Benson <louise.benson@gmail.com>
2023-10-09 18:50:05 +0000

    Add add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from products.models import Product
+
 
 # Create your views here.
 
@@ -6,3 +9,20 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add specified product to the shopping bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = 1
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    bag[item_id] = quantity
+
+    messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['bag'] = bag
+
+    return redirect(redirect_url)
