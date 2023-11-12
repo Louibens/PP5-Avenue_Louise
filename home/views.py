59c255fe4de960f2e1ab01e5992c5609911f79a9
Louise Benson <louise.benson@gmail.com>
2023-11-12 18:44:23 +0000

    Add testimonial carousel to homepage

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from products.models import Product
+from testimonials.models import Testimonial
 
 # Create your views here.
 
 def Index(request):
     """ A view to return the index page """
     products = Product.objects.order_by('-id')[:3]
-    context = {'products': products}
+    testimonials = Testimonial.objects.all
+    context = {
+        'products': products,
+        'testimonials': testimonials}
     return render(request, 'home/index.html', context)
 
