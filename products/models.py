26026807698db2f26b1d93ad8446918b757c5765
Louise Benson <louise.benson@gmail.com>
2023-11-24 17:49:56 +0000

    Update Product model to increase max digits on height and width

--- products/models.py
+++ products/models.py
@@ -20,8 +20,8 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    height = models.DecimalField(max_digits=4, decimal_places=2)
-    width = models.DecimalField(max_digits=4, decimal_places=2)
+    height = models.DecimalField(max_digits=6, decimal_places=2)
+    width = models.DecimalField(max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = ResizedImageField(
